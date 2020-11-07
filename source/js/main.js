360e42d5c008049e5db03044722ec3ef4ffc6987
Niurakha <proshinaann89@gmail.com>
2020-11-07 21:40:52 +0300

    Исправляет замечания

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// var logo = document.querySelector('.header-top__logo-svg');
 var header = document.querySelector('.header-top');
 var navToggle = document.querySelector('.main-nav__toggle');
 
@@ -12,10 +11,8 @@ navToggle.addEventListener('click', function () {
   if (header.classList.contains('header-top--opened')) {
     header.classList.remove('header-top--opened');
     header.classList.add('header-top--closed');
-    // logo.classList.add('header-top__logo-svg--closed');
   } else {
     header.classList.add('header-top--opened');
     header.classList.remove('header-top--closed');
-    // logo.classList.remove('header-top__logo-svg--closed');
   }
 });
